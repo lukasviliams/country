23ce3b90e8a8e73c78e810a853e67670c341168c
Lukáš Viliams <lukas.viliams@gmail.com>
2022-03-12 09:27:28 +0100

    dark mode solor switch completed

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="router-header">
         <router-link to="/">Where in the World</router-link>
       </div>
-      <div class="svg_container">
+      <div class="svg_container" @click="toggleColor">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="25"
@@ -28,6 +28,56 @@
     <router-view />
   </div>
 </template>
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const darkMode = ref(false);
+
+    // Switch color modes
+    const toggleColor = () => {
+      // /////////////////////Dark mode //////////////////////////////
+      darkMode.value = !darkMode.value;
+      if (darkMode.value) {
+        // Root --dark-blue
+        document.documentElement.style.setProperty(
+          "--dark-blue",
+          "rgba(255, 255, 255, 1)"
+        );
+        // Root --light-mode-back
+        document.documentElement.style.setProperty(
+          "--light-mode-back",
+          "rgba(32, 44, 55, 1)"
+        );
+        // Root -white
+        document.documentElement.style.setProperty(
+          "--white",
+          "rgba(43, 57, 69, 1)"
+        );
+      }
+      // /////////////////////light mode //////////////////////////////
+      else {
+        document.documentElement.style.setProperty(
+          // Root --dark-blue
+          "--dark-blue",
+          "rgba(43, 57, 69, 1)"
+        );
+        // Root --light-mode-back
+        document.documentElement.style.setProperty(
+          "--light-mode-back",
+          "rgba(250, 250, 250, 1)"
+        );
+        // Root -white
+        document.documentElement.style.setProperty(
+          "--white",
+          "rgba(255, 255, 255, 1)"
+        );
+      }
+    };
+    return { darkMode, toggleColor };
+  },
+};
+</script>
 
 <style>
 .navigation {
@@ -41,15 +91,21 @@
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
 }
 .svg_container svg {
   margin-right: 2rem;
+}
+svg path {
+  fill: rgba(255, 255, 255, 1);
 }
 /* #nav a {
   font-weight: bold;
   color: #2c3e50;
 } */
-
+.body_text-semi {
+  color: var(--dark-blue);
+}
 #nav a.router-link-exact-active,
 #nav a {
   font-family: "Nunito", sans-serif;
